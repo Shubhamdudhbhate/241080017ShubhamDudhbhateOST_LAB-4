3edf8c6e05701ee66d3c78d4dde074443ae7a874
shubhamdudhbhate <shubhamdudhbhate2006@gmail.com>
2025-08-12 05:13:14 -0400

    Adding Three cpp Files

---
+++ ReverseElementinArray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the " << n << " elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n / 2; i++) {
+        swap(arr[i], arr[n - i - 1]);
+    }
+
+    cout << "Reversed array:\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
